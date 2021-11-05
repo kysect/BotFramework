52f19dc13dbcba75267c6250f98e4d57083ca244
Bibletoon <alex8.ivanov@gmail.com>
2021-11-05 11:47:38 +0300

    Feat: add attribute cache

--- Kysect.BotFramework/Core/Tools/Extensions/GetAttributeExtensions.cs
+++ Kysect.BotFramework/Core/Tools/Extensions/GetAttributeExtensions.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using Kysect.BotFramework.Core.Commands;
 
 namespace Kysect.BotFramework.Core.Tools.Extensions
 {
     public static class GetAttributeExtensions
     {
-        public static BotCommandDescriptorAttribute GetBotCommandDescriptorAttribute(this Type t)
-            => Attribute.GetCustomAttribute(t, typeof(BotCommandDescriptorAttribute)) as BotCommandDescriptorAttribute;
+        private static readonly ConcurrentDictionary<Type, BotCommandDescriptorAttribute> Attributes = new ();
+        
+        public static BotCommandDescriptorAttribute GetBotCommandDescriptorAttribute(this Type type)
+            => Attributes.GetOrAdd(
+                type,
+                t => Attribute.GetCustomAttribute(t, typeof(BotCommandDescriptorAttribute))
+                    as BotCommandDescriptorAttribute);
     }
 }
