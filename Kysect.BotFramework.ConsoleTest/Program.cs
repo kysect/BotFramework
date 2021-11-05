f140478f00d1ce28f617a5e9815a29053ca48f3e
Fredi Kats <fredikats@outlook.com>
2021-11-05 08:57:38 +0300

    fix: remove dependency to EFCore.SQLite

--- Kysect.BotFramework.ConsoleTest/Program.cs
+++ Kysect.BotFramework.ConsoleTest/Program.cs
@@ -1,21 +1,32 @@
+using System;
 using System.Threading.Tasks;
 using Kysect.BotFramework.ApiProviders.Telegram;
-using Kysect.BotFramework.Commands;
 using Kysect.BotFramework.Core;
+using Kysect.BotFramework.DefaultCommands;
 using Kysect.BotFramework.Settings;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kysect.BotFramework.ConsoleTest
 {
     public static class Program
     {
+        public static string TelegramToken
+        {
+            get
+            {
+                throw new Exception("Set token");
+                return string.Empty;
+            }
+        }
+
         private static async Task MainAsync()
         {
-            var telegramToken = "1484135943:AAGM6JHSj-ER8ekwHQbaJPxHDxfCEz1hSHM";
 
-            var settings = new ConstSettingsProvider<TelegramSettings>(new TelegramSettings(telegramToken));
+            var settings = new ConstSettingsProvider<TelegramSettings>(new TelegramSettings(TelegramToken));
             var api = new TelegramApiProvider(settings);
 
             BotManager botManager = new BotManagerBuilder()
+                .SetDatabaseOptions(o => { o.UseSqlite("Filename=bf.db"); })
                 .SetPrefix('!')
                 .SetCaseSensitive(false)
                 .AddCommand(PingCommand.Descriptor)
--- Kysect.BotFramework/Core/BotManagerBuilder.cs
+++ Kysect.BotFramework/Core/BotManagerBuilder.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Kysect.BotFramework.ApiProviders;
 using Kysect.BotFramework.Core.CommandInvoking;
 using Kysect.BotFramework.Core.Commands;
+using Kysect.BotFramework.Core.Exceptions;
 using Kysect.BotFramework.Core.Tools.Loggers;
 using Kysect.BotFramework.Data;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +19,10 @@ namespace Kysect.BotFramework.Core
 
         private char _prefix = '\0';
         private bool _sendErrorLogToUser;
+
+        private bool _dbContextInitialized = false;
+
+        //FK: we should make this private, shouldn't we?
         public ServiceCollection ServiceCollection { get; } = new ServiceCollection();
 
         public BotManagerBuilder AddLogger(ILogger logger)
@@ -59,12 +65,18 @@ namespace Kysect.BotFramework.Core
             return this;
         }
 
+        public BotManagerBuilder SetDatabaseOptions(Action<DbContextOptionsBuilder> optionsAction)
+        {
+            ServiceCollection.AddDbContext<BotFrameworkDbContext>(optionsAction);
+            _dbContextInitialized = true;
+            return this;
+        }
+
         public BotManager Build(IBotApiProvider apiProvider)
         {
-            ServiceCollection.AddDbContext<BotFrameworkDbContext>(o =>
-                                                                  {
-                                                                      o.UseSqlite("Filename=bf.db");
-                                                                  });
+            if (!_dbContextInitialized)
+                throw new BotValidException("Database context options was not initialized");
+
             ServiceProvider serviceProvider = ServiceCollection.BuildServiceProvider();
             var commandHandler = new CommandHandler(serviceProvider);
             _commands.ForEach(commandHandler.RegisterCommand);
