19297d51cb1f2aa29834e50e0b7adc3c78d7e526
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-06-10 13:50:24 +0300

    refactor: dialog context SetStateAsync

--- Kysect.BotFramework/Core/Contexts/DialogContext.cs
+++ Kysect.BotFramework/Core/Contexts/DialogContext.cs
@@ -7,30 +7,46 @@ namespace Kysect.BotFramework.Core.Contexts
 {
     public class DialogContext
     {
-        public int State { get; set; }
-        private readonly ContextType _contextType;
+        private readonly BotFrameworkDbContext _dbContext;
         private readonly long _senderInfoId;
+
+        private int? _state;
+
         public SenderInfo SenderInfo { get; }
 
-        public DialogContext(ContextType contextType, SenderInfo senderInfo)
-        {
-            _contextType = contextType;
-            SenderInfo = senderInfo;
-        }
+        public DialogContext(SenderInfo senderInfo)
+            => SenderInfo = senderInfo;
 
-        public DialogContext(int state, long senderInfoId, ContextType contextType, SenderInfo senderInfo)
-            : this(contextType, senderInfo)
+        public DialogContext(int state, long senderInfoId, SenderInfo senderInfo, BotFrameworkDbContext dbContext)
+            : this(senderInfo)
         {
-            State = state;
             _senderInfoId = senderInfoId;
+            _state = state;
+            _dbContext = dbContext;
         }
 
-        internal async Task SaveChangesAsync(BotFrameworkDbContext dbContext)
+        public int? GetState() => _state;
+
+        public async Task SetStateAsync(int state)
         {
-            DialogContextEntity context = dbContext.DialogContexts.FirstOrDefault(x => x.SenderInfoId == _senderInfoId && x.ContextType == _contextType);
-            context.State = State;
-            dbContext.DialogContexts.Update(context);
-            await dbContext.SaveChangesAsync();
+            _state = state;
+
+            if (_dbContext is not null)
+            {
+                await SaveChangesAsync();
+            }
+        }
+
+        private async Task SaveChangesAsync()
+        {
+            DialogContextEntity context = _dbContext.DialogContexts.FirstOrDefault(
+                x =>
+                    x.SenderInfoId == _senderInfoId
+                    && x.ContextType == SenderInfo.ContextType);
+
+            context!.State = _state!.Value;
+            _dbContext.DialogContexts.Update(context);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
