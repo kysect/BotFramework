686ba829c539032fb24522979b27c01161748fb1
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-07-20 14:57:24 +0300

    fix: add context provider to abstactions

---
+++ Kysect.BotFramework.Abstractions/Abstractions/Services/IDialogContextProvider.cs
@@ -0,0 +1,8 @@
+﻿using Kysect.BotFramework.Abstractions.Contexts;
+
+namespace Kysect.BotFramework.Abstactions.Services;
+
+public interface IDialogContextProvider
+{
+    public IDialogContext GetDialogContext(ISenderInfo senderInfo);
+}
