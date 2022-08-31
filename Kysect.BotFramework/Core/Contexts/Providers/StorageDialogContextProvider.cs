33934b13b6dcb115dd5ce6e5f43f1ec77601dacd
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-31 23:25:21 +0300

    refactor: add visitor usage

--- Kysect.BotFramework/Core/Contexts/Providers/StorageDialogContextProvider.cs
+++ Kysect.BotFramework/Core/Contexts/Providers/StorageDialogContextProvider.cs
@@ -1,16 +1,18 @@
 ﻿using Kysect.BotFramework.Abstactions.Services;
 using Kysect.BotFramework.Abstractions.Contexts;
-using Kysect.BotFramework.Data;
+using Kysect.BotFramework.Abstractions.Visitors;
 
 namespace Kysect.BotFramework.Core.Contexts.Providers;
 
 public class StorageDialogContextProvider : IDialogContextProvider
 {
-    private readonly BotFrameworkDbContext _dbContext;
+    private readonly IContextVisitor<IDialogContext> _dialogContextVisitor;
 
-    public StorageDialogContextProvider(BotFrameworkDbContext dbContext)
-        => _dbContext = dbContext;
+    public StorageDialogContextProvider(IContextVisitor<IDialogContext> dialogContextVisitor)
+    {
+        _dialogContextVisitor = dialogContextVisitor;
+    }
 
     public IDialogContext GetDialogContext(ISenderInfo senderInfo)
-        => (senderInfo as SenderInfo).GetOrCreateDialogContext(_dbContext);
+        => senderInfo.Accept(_dialogContextVisitor);
 }
